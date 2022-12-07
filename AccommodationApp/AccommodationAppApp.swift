8fd5c6ad856cc33a945c06bce6dad9e64eeabf62
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2022-12-07 12:51:24 +0100

    Initial Commit

---
+++ AccommodationApp/AccommodationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccommodationAppApp.swift
+//  AccommodationApp
+//
+//  Created by Lorenzo Brescanzin on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AccommodationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
