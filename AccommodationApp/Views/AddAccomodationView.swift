8b78baf5c73ec8d967ca25c8e4df82a330c2d5a2
levelupwiththecode <massidedosso@gmail.com>
2022-12-09 14:11:57 +0100

    Creation of form

--- AccommodationApp/Views/AddAccomodationView.swift
+++ AccommodationApp/Views/AddAccomodationView.swift
@@ -10,12 +10,155 @@ import SwiftUI
 struct AddAccomodationView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State private var address = ""
+    @State private var description = ""
+    @State private var rent  = ""
+    @State private var extraCost = ""
+    @State private var deposit = ""
+    @State private var platformAgencyFees = ""
+    @State private var possibilityToVisit = false
+    @State private var dateOfVisit = Date()
+    @State private var hourOfVisit = Date()
+    @State private var urlAdvert = ""
+    @State private var ownerFlatName = ""
+    @State private var ownerFlatPhone = ""
+    @State private var flatExtraDetails = ""
+    @State private var selectedTypeOfAccomodation: TypeOfAccomodation = .room
+    @State private var selectedTypeOfContact: TypeOfContact = .individual
+    
+    enum TypeOfAccomodation: String, CaseIterable, Identifiable {
+        case room, studio, entireFlat
+        var id: Self { self }
+    }
+    enum TypeOfContact: String, CaseIterable, Identifiable {
+        case individual, professional, bookingPlatform
+        var id: Self { self }
+    }
+    enum Status: String, CaseIterable, Identifiable {
+        case toContact, toVisit, filePreparation, fileSubmitted, bookingSubmitted, awaitingReply, accepted, rejected
+        var id: Self { self }
+    }
+    @State private var selectedStatus: Status = .toContact
+
+    
     
     var body: some View {
         
         NavigationView{
-            Text("hhh")
-            
+            Form {
+                
+                Group {
+                    Section(header: Text("Address")) {
+                        TextField("", text: $address)
+                    }
+                    Section(header: Text("Description")){
+                        TextField("", text: $description )
+                    }
+                    Section(header: Text("Type of accomodation")) {
+                        
+                         List {
+                             Picker("", selection: $selectedTypeOfAccomodation) {
+                                 Text("Room in house to share").tag(TypeOfAccomodation.room)
+                                 Text("Studio").tag(TypeOfAccomodation.studio)
+                                 Text("Entire flat").tag(TypeOfAccomodation.entireFlat)
+                             }
+                         }
+                    }
+                    
+                   
+                    Section(header: Text("Rent")) {
+                        TextField("",  text: $rent )
+                    }
+                    // - UPLOAD : Photo
+                }
+                
+                Group {
+                    Section(header: Text("Extra cost bills")) {
+                        TextField("", text: $extraCost )
+                    }
+                    Section(header: Text("Deposit")) {
+                        TextField("", text: $deposit )
+                    }
+                    Section(header: Text("Platform / Agency fees")) {
+                        TextField("", text: $platformAgencyFees )
+                    }
+                    
+                }
+                
+                Section(header: Text("Possibility to visit the room")) {
+                    // - TOGGLE : possibility to visit the room
+                    Toggle(""    , isOn: $possibilityToVisit)
+                }
+                
+                
+                Group {
+                    Section(header: Text("Date of visit")) {
+                        DatePicker("", selection: $dateOfVisit, displayedComponents: .date)
+                    }
+                    Section(header: Text("Hour of visit")) {
+                        DatePicker("", selection: $hourOfVisit, displayedComponents: .hourAndMinute)
+                    }
+                    
+                    
+                    
+                } // (Image(systemName: "calendar"))
+                
+                
+                Group {
+                    Section(header: Text("Link to the advert")) {
+                        TextField("", text: $urlAdvert )
+                    }
+                    
+                    Section(header: Text("Type of contact")) {
+                        
+                         List {
+                             Picker("", selection: $selectedTypeOfContact) {
+                                 Text("Individual").tag(TypeOfContact.individual)
+                                 Text("Professional (real estate agency").tag(TypeOfContact.professional)
+                                 Text("Booking platform").tag(TypeOfContact.bookingPlatform)
+                             }
+                         }
+                    }
+                    
+                    Section(header: Text("Name of contact")) {
+                        TextField("", text: $ownerFlatName)
+                    }
+                    Section(header: Text("Phone")) {
+                        TextField("", text: $ownerFlatPhone)
+                    }
+                    
+                    
+                    Section(header: Text("Status")) {
+                        
+                         List {
+                             Picker("", selection: $selectedStatus) {
+                                 Text("To contact").tag(Status.toContact)
+                                 Text("To Visit").tag(Status.toVisit)
+                                 Text("File preparation").tag(Status.filePreparation)
+                                 Text("File submitted").tag(Status.fileSubmitted)
+                                 Text("Booking submitted").tag(Status.bookingSubmitted)
+                                 Text("Awaiting reply").tag(Status.awaitingReply)
+                                 Text("Accepted").tag(Status.accepted)
+                                 Text("Rejected").tag(Status.rejected)
+                                
+                             }
+                         }
+                    }
+                    
+                    
+                    
+                    
+                    // - LIST OF SINGLE SELECT : Status
+                    Section(header: Text("Other")) {
+                        TextField("", text: $flatExtraDetails)
+                    }
+                } // end of last Group
+                
+
+                
+                
+                
+            }
             
                 .navigationTitle("Add a new Accomodation")
                 .navigationBarTitleDisplayMode(.inline)
@@ -31,7 +174,7 @@ struct AddAccomodationView: View {
                     ToolbarItem(placement: .confirmationAction){
                         Button(action: {}, label:
                                 {
-                            Text("Save")
+                            Text("Add")
                         })
                     }
                 }
