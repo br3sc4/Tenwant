efa5e8d47b926ac8e752dc088468ffc492cf69bd
Antonella Giugliano <giuglianoanton@gmail.com>
2022-12-10 21:13:28 +0100

    add the new Accommodation attribute for the scheduled appointment struct in the coretestview

--- AccommodationApp/CoreData/CoreDataTestView.swift
+++ AccommodationApp/CoreData/CoreDataTestView.swift
@@ -27,7 +27,7 @@ struct CoreDataTestView: View {
                                                     description_text: "2 room Appartement 125m2 in Centro Storico",
                                                     rent_cost: 1200,
                                                     extra_cost: 70,
-                                                    url: "https://www.idealista.it/de/immobile/25939751/", isFavourite: false)
+                                                    url: "https://www.idealista.it/de/immobile/25939751/", isFavourite: false, scheduled_appointment: Date.now)
             }
             List {
                 ForEach(accomodations) { accomodation in
