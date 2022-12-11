8f8dc79e31ecd7524381585cd8c6c891c37cdc6a
Antonella Giugliano <giuglianoanton@gmail.com>
2022-12-11 22:10:07 +0100

    fix the ui for the list and the rows

--- AccommodationApp/Components/MyCalendarRowView.swift
+++ AccommodationApp/Components/MyCalendarRowView.swift
@@ -13,64 +13,75 @@ struct MyCalendarRowView: View {
     
     
     var body: some View {
-        VStack(alignment: .leading){
+        
+        if let appointment = accommodation.scheduled_appointment{
             
-            ZStack{
+            ZStack(alignment: .center){
                 
-            
-                ZStack(alignment: .topLeading){
                 Rectangle()
-//                    .foregroundColor(.white)
-                    .foregroundColor(.gray)
-//                    .cornerRadius(14)
-//                    .shadow(radius: 1)
-                    .frame(width: 345, height: 70)
-                    .opacity(0.1)
-                    .padding(.leading, -8)
-//
-                Rectangle()
-                    .frame(width: 5, height: 70)
-                    .foregroundColor(.gray)
-                    
-            }
-//            .mask(Rectangle()
-//                .frame(width: 345, height: 70)
-//                .cornerRadius(14)
-//                   )
+//                                    .foregroundColor(.white)
+                    .foregroundColor(.secondary)
+                    .cornerRadius(4.5)
+                    .shadow(radius: 1)
+                    .frame(maxWidth: .infinity)
+                    .frame(minHeight: 45)
+                    .opacity(0.2)
+//                    .padding(.leading, -8)
                 
-                if let appointment = accommodation.scheduled_appointment{
-                    VStack(alignment: .leading, spacing: 5){
-                        
-                        HStack {
-                            Text(accommodation.title ?? accommodation.wrappedTitle)
-                                .lineLimit(1)
-                                .foregroundColor(.primary)
-                                .font(.system(size: 12))
-                                .bold()
-                            
-                            Spacer()
+                Rectangle()
+//                                    .foregroundColor(.white)
+                    .foregroundColor(.secondary)
+//                    .cornerRadius(10)
+                    .shadow(radius: 1)
+                    .frame(width: 2)
+                    .frame(minHeight: 9)
+                    .opacity(0.3)
+                        .padding(.leading, -100)
+                        .padding([.bottom, .top], 6)
+
+                
+                    HStack(alignment: .center, spacing: 25) {
+                       
                             Text(appointment.formatted(date: .omitted, time: .shortened))
                                 .foregroundColor(.primary)
                                 .font(.system(size: 12))
                                 .bold()
-                            
+                                .font(.subheadline)
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            if let title = accommodation.title{
+                                Text(title)
+                                    .lineLimit(1)
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 12))
+                                    .font(.subheadline)
+//                                    .padding([.bottom, .top], 1)
+                            }
+                            if let contactName = accommodation.contact_name{
+                                Text(contactName)
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 12))
+                                    .font(.subheadline)
+//                                    .padding([.bottom, .top], 1)
+                            }
+                            if let contactPhone = accommodation.contact_phone{
+                                Text(contactPhone)
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 12))
+                                    .font(.subheadline)
+//                                    .padding([.bottom, .top], 1)
+                            }
                         }
-                        Text(accommodation.contact_name ?? "")
-                            .foregroundColor(.secondary)
-                            .font(.system(size: 12))
-                        
-                        Text(accommodation.contact_phone ?? "")
-                            .foregroundColor(.secondary)
-                            .font(.system(size: 12))
-                        
-                    }.frame(width: 320, height: 70)
-                        
-                }
+                        Spacer()
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+               
             }
         }
-        
     }
+    
 }
+
 
 struct MyCalendarRowView_Previews: PreviewProvider {
     
