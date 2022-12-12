1b2f8d34ce730df7078c8f0a18386f92d046cc0c
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2022-12-12 19:08:28 +0100

    Bugs fix

--- AccommodationApp/Views/AddAccommodationView.swift
+++ AccommodationApp/Views/AddAccommodationView.swift
@@ -158,7 +158,7 @@ struct AddAccommodationView: View {
                          Button("Discard Changes", role: .destructive) {
                              dismiss()
                          }
-                         Button("Keep Editing", role: .cancel) {}
+                         Button("Cancel", role: .cancel) {}
                       }
                 }
              
