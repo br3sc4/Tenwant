ff6054786f5c78065980c2befc12460925db4ae1
Antonella Giugliano <giuglianoanton@gmail.com>
2022-12-14 11:56:12 +0100

    fix ui action buttons

---
+++ AccommodationApp/Components/ActionButtonView.swift
@@ -0,0 +1,56 @@
+//
+//  ActionButtonView.swift
+//  AccommodationApp
+//
+//  Created by Antonella Giugliano on 14/12/22.
+//
+
+import SwiftUI
+
+struct ActionButtonView: View {
+    
+    @Environment(\.colorScheme) private var colorScheme
+    
+    var role: String
+    var symbolName: String
+    var textLabel: String
+    var action: () -> Void
+    
+    var body: some View {
+        
+        if colorScheme == .light {
+        Button(action: action) {
+            VStack(spacing: 3) {
+                Image(systemName: symbolName)
+                    .foregroundColor(role == "delete" ? .red : .accentColor)
+                
+                Text(textLabel.capitalized)
+                    .foregroundColor(role == "delete" ? .red : .accentColor)
+                    .font(.system(size: 11))
+                    .font(.subheadline)
+            }.frame(width: role == "isFavourite" ? 65 : 55)
+            .frame(width: 55, height: 50)
+        }.tint(Color(UIColor.systemBackground))
+            .buttonStyle(.borderedProminent)
+        }else{
+            Button(action: action) {
+                VStack(spacing: 3) {
+                    Image(systemName: symbolName)
+                        .foregroundColor(role == "delete" ? .red : .accentColor)
+                    
+                    Text(textLabel.capitalized)
+                        .foregroundColor(role == "delete" ? .red : .accentColor)
+                        .font(.system(size: 11))
+                        .font(.subheadline)
+                        .frame(width: role == "isFavourite" ? 65 : 55)
+                }.frame(width: 55, height: 50)
+            }.buttonStyle(BorderedButtonStyle())
+        }
+    }
+}
+
+//struct ActionButtonView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ActionButtonView()
+//    }
+//}
